d9b238ff796b5a326c650b4130648a17c9bc631b
gh1st <47651113+gh1st@users.noreply.github.com>
2022-03-05 12:38:04 +0000

    Somechanges

--- ext.go
+++ ext.go
@@ -1,8 +1,6 @@
 package caddydelay
 
 import (
-	"fmt"
-	"io"
 	"math/rand"
 	"net/http"
 	"time"
@@ -20,8 +18,6 @@ func init() {
 
 type Middleware struct {
 	Deplay string `json:"deplay,omitempty"`
-
-	w io.Writer
 }
 
 func (Middleware) CaddyModule() caddy.ModuleInfo {
@@ -31,30 +27,22 @@ func (Middleware) CaddyModule() caddy.ModuleInfo {
 	}
 }
 
-// Provision implements caddy.Provisioner.
 func (m *Middleware) Provision(ctx caddy.Context) error {
 	return nil
 }
 
-// Validate implements caddy.Validator.
 func (m *Middleware) Validate() error {
-	if m.w == nil {
-		return fmt.Errorf("no writer")
-	}
 	return nil
 }
 
-// ServeHTTP implements caddyhttp.MiddlewareHandler.
 func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 	time.Sleep(time.Duration(r1.Intn(100)+450) * time.Millisecond)
 
-	m.w.Write([]byte(r.RemoteAddr))
 	return next.ServeHTTP(w, r)
 }
 
-// UnmarshalCaddyfile implements caddyfile.Unmarshaler.
 func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
 		if !d.Args(&m.Deplay) {
